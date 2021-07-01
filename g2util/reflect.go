29efd87a7d337a8cfa8264690ebdf4b0bb78e50b
charles <linkhe@foxmail.com>
2021-07-01 10:49:25 +0800

    +++

--- g2util/reflect.go
+++ g2util/reflect.go
@@ -30,17 +30,20 @@ func NewValue(bean interface{}) (val interface{}) {
  */
 func ObjectTagInstances(obj interface{}, tagName string) []interface{} {
 	data := make([]interface{}, 0)
+	tv1 := ValueIndirect(reflect.ValueOf(obj))
 	_f1append := func(vv reflect.Value, vf reflect.StructField) {
 		_, has := vf.Tag.Lookup(tagName)
 		if !has {
 			return
 		}
-		if !(vv.CanSet() && vv.CanAddr() && !vv.IsNil() && vv.Kind() == reflect.Ptr) {
+		if !(vv.CanSet() && vv.CanAddr() && vv.Kind() == reflect.Ptr) {
 			return
+		}
+		if vv.IsNil() {
+			vv.Set(reflect.New(vf.Type.Elem()))
 		}
 		data = append(data, vv.Interface())
 	}
-	tv1 := ValueIndirect(reflect.ValueOf(obj))
 	for i := 0; i < tv1.NumField(); i++ {
 		_f1append(tv1.Field(i), tv1.Type().Field(i))
 	}
